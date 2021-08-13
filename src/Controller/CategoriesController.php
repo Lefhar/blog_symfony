ffed559824199827676500b4beda2113e81c69f7
harold <s.lefebvre907@laposte.net>
2021-08-13 13:38:08 +0200

    blog symfony

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -5,19 +5,22 @@ use App\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 class CategoriesController extends  AbstractController
 {
     /**
-     * @Route("/categories", name="category")
+     * @Route("/categories/{slug}", name="category")
      */
-    public function index(): Response
+    public function index(Request $request): Response
     {
         // affichage de la page categorie
         $repo = $this->getDoctrine()->getRepository(Category::class);
+        $category = $repo->findBy(['name'=>$request]);
         $categories = $repo->findAll();
         return $this->render('categories/index.html.twig', [
             'controller_name' => 'CategoriesController',
-            'category'=> $categories
+            'category'=> $category,
+            'categories'=> $categories
         ]);
     }
 }
